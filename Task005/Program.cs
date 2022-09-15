c22201abac870bd11212fe0f58cbd3971ae63834
Victor Rusaev <rusaev74@mail.ru>
2022-09-15 22:33:34 +0300

    Task005

--- Task005/Program.cs
+++ Task005/Program.cs
@@ -5,8 +5,6 @@ string A = Console.ReadLine();
 int numberA = int.Parse(A);
 numberA = Math.Abs(numberA);
 
-Console.WriteLine(numberA % 10);
-
 if((numberA > 99) & (numberA < 1000))
 {
     Console.WriteLine(numberA % 10);
